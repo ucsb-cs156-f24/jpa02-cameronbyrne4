c685bd0bda6aaa6249c52af7fa3bc33bca3af093
Cameron B <cameronbyrne@ucsb.edu>
2024-10-09 11:38:23 -0700

    xy - updated with actual developer name

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,12 +22,26 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-f24-teams>
-        assertEquals("Chris G.", Developer.getName());
+        assertEquals("Cameron", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_id() {
+        assertEquals("cameronbyrne4", Developer.getGithubId());
+    }
 
+    @Test
+    public void getTeam_returns_correct_team() {
+        Team team = Developer.getTeam();
+        assertEquals("f24-01", team.getName());
+        // Use contains to check for names, as order is not guaranteed
+        assertTrue(team.getMembers().contains("Vincent"));
+        assertTrue(team.getMembers().contains("Viraj"));
+        assertTrue(team.getMembers().contains("Scott"));
+        assertTrue(team.getMembers().contains("Raymond"));
+        assertTrue(team.getMembers().contains("Jun"));
+        assertTrue(team.getMembers().contains("Cameron"));
+    }
 }
